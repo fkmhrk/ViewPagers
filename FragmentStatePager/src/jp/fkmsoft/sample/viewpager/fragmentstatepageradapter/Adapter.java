079fb429a1c7c8763f87f4348e811c94413c6581
fkm <fkm@fkmsoft.jp>
2012-10-04 22:51:09 +0900

    add samle of FragmentStatePagerAdapter

---
+++ FragmentStatePager/src/jp/fkmsoft/sample/viewpager/fragmentstatepageradapter/Adapter.java
@@ -0,0 +1,35 @@
+package jp.fkmsoft.sample.viewpager.fragmentstatepageradapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class Adapter extends FragmentStatePagerAdapter {
+
+    private List<Fragment> items = new ArrayList<Fragment>();
+    public Adapter(FragmentManager fm) {
+        super(fm);
+    }
+    
+    public void add(Fragment item) {
+        items.add(item);
+    }
+    
+    public void remove(int position) {
+        items.remove(position);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+}
---
+++ FragmentStatePager/src/jp/fkmsoft/sample/viewpager/fragmentstatepageradapter/MainActivity.java
@@ -0,0 +1,32 @@
+package jp.fkmsoft.sample.viewpager.fragmentstatepageradapter;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // set adapter
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        
+        setButtonListener();
+    }
+
+    private void setButtonListener() {
+        ButtonListener listener = new ButtonListener(this);
+        int[] ids = {R.id.button_add, R.id.button_remove};
+        for (int id : ids) {
+            View button = findViewById(id);
+            button.setOnClickListener(listener);
+        }
+        
+    }
+}
